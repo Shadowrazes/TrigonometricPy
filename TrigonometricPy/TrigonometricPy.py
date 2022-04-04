56cd47cc0114744fc4ae436654db8f060c177013
Михаил <hugehug@mail.ru>
2022-04-04 23:04:41 +0700

    add graph

--- TrigonometricPy/TrigonometricPy.py
+++ TrigonometricPy/TrigonometricPy.py
@@ -1,4 +1,6 @@
 import math, cmath
+import matplotlib.pyplot as plt
+import numpy as np
 
 class FuncEq:
     def __init__(self, x, y):
@@ -21,10 +23,27 @@ def Y_x(data, T, x, n):
         sum += A(data, i, n) * c
     return sum / n
 
-data = [FuncEq(0, 0), FuncEq(math.pi / 2, 1), FuncEq(math.pi, 0), FuncEq(3 * math.pi / 2, -1), FuncEq(2 * math.pi, 0)]
-T = (len(data) - 1) * math.pi / 2 
-x =  math.pi / 4
+def graph(X, Y, descr, figureNum):
+    plt.figure(figureNum)
+    plt.plot(X, Y, label = descr)
+    plt.legend()
+
+data = [FuncEq(0, 0), FuncEq(math.pi / 4, 1), FuncEq(math.pi / 2, 0), FuncEq(3 * math.pi / 4, -1), FuncEq(math.pi, 0)]
+T = data[len(data) - 1].x - data[0].x
+x =  1
 n = len(data) - 1
 y = Y_x(data, T, x, n)
-print("y(", x, ") = ",y.real)
-print("sin(", x, ") = ",math.sin(x))
+print("y(2 * ", x, ") = ",y.real)
+print("sin(2 * ", x, ") = ",math.sin(2 * x))
+
+X = np.arange(0, 2 * math.pi, 0.1)
+Y = []
+sin = []
+
+for i in range(len(X)):
+    Y.append(Y_x(data, T, X[i], n).real)
+    sin.append(math.sin(2 * X[i]))
+    
+graph(X, Y, "intrp sin(2x)", 1)
+graph(X, sin, "sin(2x)", 2)
+plt.show()
